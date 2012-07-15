24390d1c556da1f37367530e6bf14ed633ec8839
mattsmith118 <mattsmith118@gmail.com>
2012-07-15 01:05:27 -0700

    in process changes - after midnight

--- 3taps-CSharp-Client/com/threetaps/dto/search/CountResponse.cs
+++ 3taps-CSharp-Client/com/threetaps/dto/search/CountResponse.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,17 +10,4 @@ namespace com.threetaps.dto.search
         public int count { get; set; }
     }
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
-namespace com.threetaps.dto.search
-{
-    public class CountResponse
-    {
-        private int count { get; set; }
-    }
-}
->>>>>>> 09dc52575275e41681f491772759c34944940bb8
