f1e1f70fb202e45ed0c6ab0364f278b4a7083374
David Brown <lecithin@gmail.com>
2012-07-14 22:57:23 -0700

    Ports complete for util directory

--- 3taps-CSharp-Client/com/util/Constants.cs
+++ 3taps-CSharp-Client/com/util/Constants.cs
@@ -4,9 +4,9 @@ namespace ThreeTaps
 {
 	public class Constants
 	{
-		public Constants ()
-		{
-		}
+		public static String DEFAULT_API_URL = "http://3taps.net";
+		public static int DEFAULT_API_PORT = 80;
+		public String DATE_FORMAT = "yyyy/MM/dd HH:mm:ss z";
 	}
 }
 
--- 3taps-CSharp-Client/com/util/Utils.cs
+++ 3taps-CSharp-Client/com/util/Utils.cs
@@ -4,8 +4,20 @@ namespace ThreeTaps
 {
 	public class Utils
 	{
-		public Utils ()
+		public static String join(List<String> strings)
 		{
+			if (strings != null && strings.size() > 0)
+			{
+				StringBuffer joinBuffer = new StringBuffer();
+				foreach (String iter_string in strings)
+				{
+					joinBuffer.append(iter_string);
+					joinBuffer.append(",");
+				}
+				joinBuffer.deleteCharAt(joinBuffer.length() - 1);
+				return joinBuffer.toString();
+			}
+			return "";
 		}
 	}
 }
