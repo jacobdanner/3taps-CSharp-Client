e864549ace0cabb95415d2019f604cd6cad8d754
Jacob Danner <jacob.danner@gmail.com>
2012-07-15 21:28:17 -0700

    Initial half hacky not working commit

---
+++ 3taps-CSharp-Client/Threetaps/client/Client.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ThreeTaps
+{
+  class Client
+  {
+      private static string DEFAULT_URL = "";
+  }
+}
---
+++ 3taps-CSharp-Client/Threetaps/client/ThreetapsClient.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Threetaps.client
+{
+  public class ThreetapsClient
+  {
+    private static ThreetapsClient instance;
+	
+	public final static String AUTH_ID_KEY = "authID";
+	private String authID;
+	
+	private ReferenceClient referenceClient;
+	private PostingClient postingClient;
+	private SearchClient searchClient;
+	private GeocoderClient geocoderClient;
+	private StatusClient statusClient;
+	
+	private ThreetapsClient() {
+		referenceClient = ReferenceClient.getInstance();
+		postingClient = PostingClient.getInstance();
+		searchClient = SearchClient.getInstance();
+		geocoderClient = GeocoderClient.getInstance();
+		statusClient = StatusClient.getInstance();
+	}
+	
+	public synchronized static ThreetapsClient getInstance() {
+		if (instance == null) instance = new ThreetapsClient();
+		return instance;
+	}
+	
+	public String getAuthID() {
+		return authID;
+	}
+	
+	public ThreetapsClient setAuthID(String authID) {
+		this.authID = authID;
+		return this;
+	}	
+	
+	public ReferenceClient getReferenceClient() {
+		return referenceClient;
+	}
+	
+	public PostingClient getPostingClient() {
+		return postingClient;
+	}
+	
+	public SearchClient getSearchClient() {
+		return searchClient;
+	}
+	
+	public GeocoderClient getGeocoderClient() {
+		return geocoderClient;
+	}
+	
+	public StatusClient getStatusClient() {
+		return statusClient;
+	}
+
+  }
+}
