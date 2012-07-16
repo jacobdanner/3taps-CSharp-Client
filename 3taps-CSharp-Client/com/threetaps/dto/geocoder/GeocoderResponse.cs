f4f48bb80e7f79b2ff7cfaf0944bf2988dfd5060
Jacob Danner <jacob.danner@gmail.com>
2012-07-15 22:56:20 -0700

    more POCOs for the client

---
+++ 3taps-CSharp-Client/com/threetaps/dto/geocoder/GeocoderResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ThreeTaps.com.threetaps.dto.geocoder
+{
+    class GeocoderResponse
+    {
+        private string code { get; set; }
+        private float latitude { get; set; }
+        private float longitude { get; set; }
+    }
+}
---
+++ 3taps-CSharp-Client/com/threetaps/dto/posting/CreateResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ThreeTaps.com.threetaps.model;
+
+namespace ThreeTaps.com.threetaps.dto.posting
+{
+    class CreateResponse
+    {
+        private string postKey { get; set; }
+        private Message error { get; set; }
+    }
+}
